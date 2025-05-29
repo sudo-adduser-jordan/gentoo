3e4c27edfac1d52faf76da388a6d868933cd3826
lostcoastwizard <lostcoastwizard@gmail.com>
2025-05-28 23:30:36 -0700

    Sync

--- appimage-install.sh
+++ appimage-install.sh
@@ -20,39 +20,39 @@ prompt_user() {
 echo "Welcome to the AppImage installer wizard!"
 
 # Prompt user for AppImage file path
-appimage_path=$(prompt_user "Enter the path to the AppImage file")
+APPIMAGE_PATH=$(prompt_user "Enter the path to the AppImage file")
 
 # Prompt user for installation directory
-install_dir=$(prompt_user "Enter the installation directory" "$HOME/Applications")
+INSTALL_DIR=$(prompt_user "Enter the installation directory" "$HOME/Applications")
 
 # Prompt user for the name of the program
-program_name=$(prompt_user "Enter the name of the program" "$(basename "$appimage_path" | cut -d. -f1)")
+PROGRAM_NAME=$(prompt_user "Enter the name of the program" "$(basename "$APPIMAGE_PATH" | cut -d. -f1)")
 
 # Set the installation path
-full_path="$install_dir/$program_name"
+FULL_PATH="$INSTALL_DIR/$PROGRAM_NAME"
 
 # Ensure the installation directory exists
-mkdir -p "$install_dir"
+mkdir -p "$INSTALL_DIR"
 
 # Copy AppImage to installation directory
-cp "$appimage_path" "$full_path"
+cp "$APPIMAGE_PATH" "$FULL_PATH"
 
 # Set execute permissions
-chmod +x "$full_path"
+chmod +x "$FULL_PATH"
 
 # Create a symbolic link in /usr/local/bin
-sudo ln -s "$full_path" "/usr/local/bin/$program_name"
+sudo ln -s "$FULL_PATH" "/usr/local/bin/$PROGRAM_NAME"
 
 # Create a desktop entry file
-desktop_entry="[Desktop Entry]
-Name=$program_name
-Exec=$program_name
+DESKTOP_ENTRY="[Desktop Entry]
+Name=$PROGRAM_NAME
+Exec=$PROGRAM_NAME
 Type=Application
 Categories=Utility;
 "
 
-desktop_entry_path="$HOME/.local/share/applications/$program_name.desktop"
-echo "$desktop_entry" > "$desktop_entry_path"
+DESKTOP_ENTRY_PATH="$HOME/.local/share/applications/$PROGRAM_NAME.desktop"
+echo "$DESKTOP_ENTRY" > "$DESKTOP_ENTRY_PATH"
 
 # Display success messages
 print_success "AppImage installed successfully!"
@@ -60,5 +60,5 @@ print_success "Symbolic link created in /usr/local/bin."
 print_success "Program added to the start menu."
 
 # Provide information to the user
-echo -e "\nYou can run the program using the terminal command: \e[1m$program_name\e[0m"
-echo -e "You can also find it in the start menu or by searching for \e[1m$program_name\e[0m."
+echo -e "\nYou can run the program using the terminal command: \e[1m$PROGRAM_NAME\e[0m"
+echo -e "You can also find it in the start menu or by searching for \e[1m$PROGRAM_NAME\e[0m."
